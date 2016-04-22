3d40e0bc49e13a1a4761a478ee672e5e6f177550
completeconcordance <inquire@polychora.org>
2016-04-21 22:01:53 -0400

    mutable step

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import Criterion.Main
 import Data.Fractal1D
 
+seed :: [Float]
+seed = [1.0, 0.75.. -1.0]
+
+generations :: Int
+generations = 5
+
+main :: IO ()
 main = defaultMain [
-  bgroup "fractal" [ bench "basic" $ whnf fractal [0.5,-0.5]
-                   -- ,
+  bgroup "fractal" [ bench "basic" $ whnf (fractal seed) generations
+                   , bench "mutable" $ whnf (fractal' seed) generations
                    ]
   ]
--- src/Data/Fractal1D.hs
+++ src/Data/Fractal1D.hs
@@ -1,21 +1,32 @@
 module Data.Fractal1D where
 
-import qualified Data.Vector.Unboxed as Vector
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 import Data.Vector.Unboxed (Vector)
 
 fractal :: [Float] -> Int -> Vector Float
-fractal = simpleStep
+fractal = mutableStep
+
+fractal' :: [Float] -> Int -> Vector Float
+fractal' = simpleStep
 
 simpleStep :: [Float] -> Int -> Vector Float
 simpleStep seed generations = (iterate step xs) !! generations
-  where xs = Vector.fromList seed
+  where xs = V.fromList seed
         step :: Vector Float -> Vector Float
-        step = Vector.concatMap (\x -> Vector.map (*x) xs)
+        step = V.concatMap (\x -> V.map (*x) xs)
 
 mutableStep :: [Float] -> Int -> Vector Float
-mutableStep seed generations = (iterate step xs) !! generations
-  where xs = Vector.fromList seed
+mutableStep seed generations = (iterate step seed') !! generations
+  where seed' = V.fromList seed
+        seedLen = V.length seed'
         step :: Vector Float -> Vector Float
-        step = Vector.concatMap (\x -> Vector.map (*x) xs)
+        step inp = V.create $ do
+          let n = V.length inp
+          v <- MV.new $ n * seedLen
+          (flip V.imapM_) inp $ \i x -> do
+            let slice = MV.unsafeSlice (i * seedLen) seedLen v
+            V.unsafeCopy slice (V.map (*x) seed')
+          return v
 
 
